fe15a937b735b5bf309b478e48c6398e0c004738
mex <mbonu.onwukwe@gmail.com>
2024-05-05 15:51:25 +0100

    Flake 8 linter fixes

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 import daemon.chat_processor_engine as cpe
 from waitress import serve
@@ -11,19 +9,22 @@ app = Flask(__name__)
 def hello_world():
     return "Hello There!"
 
+
 @app.route("/health-check")
 def health_check():
     return {"Status": "Not Implemented"}
 
-#TODO Add parameter for filename and date partition
+
+# TODO Add parameter for filename and date partition
 @app.route("/load-chat-data")
 def run_data_process():
     cpe.data_process_callback("some_random_group.txt")
     return {"Status": "Success"}
 
+
 if __name__ == '__main__':
-    PROD = 1
+    PROD = 0
     if PROD:
         serve(app, listen='*:5000')
     else:
-        app.run(debug = True,host='0.0.0.0')
+        app.run(debug=True, host='0.0.0.0')
